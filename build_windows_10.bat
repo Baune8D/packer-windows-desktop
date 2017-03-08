ccc738cbba754d63d5ac3f2fe0aadbcf822990a5
Morten Larsen <baunegaard@live.dk>
2017-03-08 16:46:06 +0100

    Upped harddisk to 256 gb

--- build_windows_10.bat
+++ build_windows_10.bat
@@ -2,5 +2,5 @@
 packer build --only=%1-iso ^
        --var iso_url=./iso/en_windows_10_multiple_editions_version_1607_updated_jan_2017_x64_dvd_9714399.iso ^
        --var iso_checksum=44064680647ee4c877a6f54739ad82759147c828 ^
-       --var disk_size=184320 ^
+       --var disk_size=262144 ^
        windows_10.json
