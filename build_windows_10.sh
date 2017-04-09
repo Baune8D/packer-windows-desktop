5b9eae619c2957fdfb30ad85bd42e9feee60d9dc
Morten Larsen <baunegaard@live.dk>
2017-04-10 00:29:42 +0200

    Fixed configuration to work with 1703

--- build_windows_10.sh
+++ build_windows_10.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 packer build --only=$1-iso \
-       --var iso_url=./iso/en_windows_10_multiple_editions_version_1607_updated_jan_2017_x64_dvd_9714399.iso \
-       --var iso_checksum=44064680647ee4c877a6f54739ad82759147c828 \
+       --var iso_url=./iso/en_windows_10_multiple_editions_version_1703_updated_march_2017_x64_dvd_10189288.iso \
+       --var iso_checksum=CE8005A659E8DF7FE9B080352CB1C313C3E9ADCE \
        --var disk_size=262144 \
        windows_10.json
