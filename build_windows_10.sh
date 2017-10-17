fbb3517e46af833fc9d707e4e38b739f669d4a58
Morten Larsen <baunegaard@live.dk>
2017-10-17 11:04:48 +0200

    Default to newer iso

--- build_windows_10.sh
+++ build_windows_10.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 packer build --only=$1-iso \
-       --var iso_url=./iso/en_windows_10_multiple_editions_version_1703_updated_june_2017_x64_dvd_10725021.iso \
-       --var iso_checksum=AB15A9CF175E8F0B8361B2D4F0EA560343B93FBE \
+       --var iso_url=./iso/en_windows_10_multiple_editions_version_1703_updated_july_2017_x64_dvd_10925340.iso \
+       --var iso_checksum=1486EC12B04D7B44BB6CB7D1D2AE52A94C891A10 \
        --var disk_size=262144 \
        windows_10.json
