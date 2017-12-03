04bec5399f98abddb57443cce6613b34aaaabf48
Morten Larsen <baunegaard@live.dk>
2017-12-03 19:59:07 +0100

    Synced @StefanScherer updates

--- build_windows_10.bat
+++ build_windows_10.bat
@@ -1,6 +1,8 @@
 @echo off
 packer build --only=%1-iso ^
-       --var iso_url=./iso/en_windows_10_multiple_editions_version_1703_updated_july_2017_x64_dvd_10925340.iso ^
-       --var iso_checksum=1486EC12B04D7B44BB6CB7D1D2AE52A94C891A10 ^
-       --var disk_size=262144 ^
-       windows_10.json
+    --var disk_type_id=3 ^
+    --var vhv_enable=true ^
+    --var disk_size=262144 ^
+    --var iso_url=./iso/en_windows_10_multi-edition_version_1709_updated_nov_2017_x64_dvd_100290211.iso ^
+    --var iso_checksum=F6E6B43F931DC41622E8AC107A5E82A3CEC7FC66 ^
+    windows_10.json
