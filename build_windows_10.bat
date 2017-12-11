75fb682fd9ef60113f3aa98dbf2ec6514fecfcf5
Morten Larsen <baunegaard@live.dk>
2017-12-11 11:30:27 +0100

    Remove redundant setting

--- build_windows_10.bat
+++ build_windows_10.bat
@@ -1,6 +1,5 @@
 @echo off
 packer build --only=%1-iso ^
-    --var vhv_enable=true ^
     --var disk_size=262144 ^
     --var iso_url=./iso/en_windows_10_multi-edition_version_1709_updated_nov_2017_x64_dvd_100290211.iso ^
     --var iso_checksum=1F958B6A80A542C82EECD918126F665AA7381D34 ^
