554631e5bbcb0743565ae7c7db07bae246e97137
Morten Larsen <baunegaard@live.dk>
2017-12-11 11:30:09 +0100

    Use growable disk for vmware

--- build_windows_10.sh
+++ build_windows_10.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 packer build --only=$1-iso \
-    --var disk_type_id=3 \
     --var vhv_enable=true \
     --var disk_size=262144 \
     --var iso_url=./iso/en_windows_10_multi-edition_version_1709_updated_nov_2017_x64_dvd_100290211.iso \
