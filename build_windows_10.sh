f03a50eeda1c8fe03a485c7b58eaf54d69240136
Morten Larsen <baunegaard@live.dk>
2018-03-24 19:13:51 +0100

    Fix: wrong checksum

--- build_windows_10.sh
+++ build_windows_10.sh
@@ -2,5 +2,5 @@
 packer build --only=$1-iso \
     --var disk_size=262144 \
     --var iso_url=./iso/en_windows_10_multi-edition_version_1709_updated_dec_2017_x64_dvd_100406711.iso \
-    --var iso_checksum=3EFB9174B7FE997509B89D0FF8CE6BDDC6000C5A \
+    --var iso_checksum=EA214EE684A5BB8230707104C54A3B74D92F1D69 \
     windows_10.json
