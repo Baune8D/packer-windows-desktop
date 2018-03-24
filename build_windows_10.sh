3da8a41c71a9a68ce345ad1bf3e0bcee02aef808
Morten Larsen <baunegaard@live.dk>
2018-03-24 19:11:28 +0100

    Update iso

--- build_windows_10.sh
+++ build_windows_10.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 packer build --only=$1-iso \
     --var disk_size=262144 \
-    --var iso_url=./iso/en_windows_10_multi-edition_version_1709_updated_nov_2017_x64_dvd_100290211.iso \
-    --var iso_checksum=1F958B6A80A542C82EECD918126F665AA7381D34 \
+    --var iso_url=./iso/en_windows_10_multi-edition_version_1709_updated_dec_2017_x64_dvd_100406711.iso \
+    --var iso_checksum=3EFB9174B7FE997509B89D0FF8CE6BDDC6000C5A \
     windows_10.json
