e733184ce663f223be83393134e73131e2d1aeaa
Morten Larsen <baunegaard@live.dk>
2018-03-26 00:20:56 +0200

    Use local Packer

--- build_windows_10.sh
+++ build_windows_10.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-packer build --only=$1-iso \
+./packer build --only=$1-iso \
     --var disk_size=262144 \
     --var iso_url=./iso/en_windows_10_multi-edition_version_1709_updated_dec_2017_x64_dvd_100406711.iso \
     --var iso_checksum=EA214EE684A5BB8230707104C54A3B74D92F1D69 \
