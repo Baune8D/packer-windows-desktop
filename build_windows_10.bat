5fb993ccdd71a4fee4245b801aece858ad6445f2
Morten Larsen <baunegaard@live.dk>
2018-05-13 19:46:58 +0200

    Update build scripts for 1803

--- build_windows_10.bat
+++ build_windows_10.bat
@@ -1,6 +1,6 @@
 @echo off
 packer build --only=%1-iso ^
     --var disk_size=262144 ^
-    --var iso_url=./iso/en_windows_10_multi-edition_version_1709_updated_dec_2017_x64_dvd_100406711.iso ^
-    --var iso_checksum=EA214EE684A5BB8230707104C54A3B74D92F1D69 ^
+    --var iso_url=./iso/en_windows_10_business_editions_version_1803_updated_march_2018_x64_dvd_12063333.iso ^
+    --var iso_checksum=28681742FE850AA4BFC7075811C5244B61D462CF ^
     windows_10.json
