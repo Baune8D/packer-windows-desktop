cad20b5fe956a6f39f9a19f4fef02d7b629145d9
Morten Larsen <baunegaard@live.dk>
2018-08-14 23:23:58 +0200

    Update build scripts

--- build_windows_10.bat
+++ build_windows_10.bat
@@ -1,6 +1,6 @@
 @echo off
 packer build --only=%1-iso ^
     --var disk_size=262144 ^
-    --var iso_url=./iso/en_windows_10_business_editions_version_1803_updated_march_2018_x64_dvd_12063333.iso ^
-    --var iso_checksum=28681742FE850AA4BFC7075811C5244B61D462CF ^
+    --var iso_url=./iso/en_windows_10_consumer_edition_version_1803_updated_jul_2018_x64_dvd_12712603.iso ^
+    --var iso_checksum=24D91524EBFC963FE428DDCAE78CB17D978B2E3F ^
     windows_10.json
