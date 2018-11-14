34b914cd67904029ed413245224eac9cee2b41b9
Morten Larsen <baunegaard@live.dk>
2018-11-14 11:43:30 +0100

    Update default iso in build

--- build_windows_10.sh
+++ build_windows_10.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 ./packer build --only=$1-iso \
     --var disk_size=262144 \
-    --var iso_url=./iso/en_windows_10_consumer_edition_version_1803_updated_jul_2018_x64_dvd_12712603.iso \
-    --var iso_checksum=24D91524EBFC963FE428DDCAE78CB17D978B2E3F \
+    --var iso_url=./iso/Win10_1809Oct_English_x64.iso \
+    --var iso_checksum=BEE211937F3ED11606590B541B2F5B97237AC09D \
     windows_10.json
