79ee31c582f03693f2bcd12bc56ae459dea39f4c
Morten Larsen <baunegaard@live.dk>
2020-12-17 09:54:41 +0100

    Fix script formatting

--- scripts/disable-winrm.ps1
+++ scripts/disable-winrm.ps1
@@ -1,7 +1,7 @@
 netsh advfirewall firewall set rule name="Windows Remote Management (HTTP-In)" new enable=yes action=block
 netsh advfirewall firewall set rule group="Windows Remote Management" new enable=yes
 $winrmService = Get-Service -Name WinRM
-if ($winrmService.Status -eq "Running"){
+if ($winrmService.Status -eq "Running") {
     Disable-PSRemoting -Force
 }
 Stop-Service winrm
