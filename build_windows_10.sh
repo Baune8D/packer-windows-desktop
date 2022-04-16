1e4d445be27993b64caa21d46a10409533df99bf
Morten Larsen <baunegaard@live.dk>
2022-04-16 20:59:07 +0200

    Update Windows 10 configs

--- build_windows_10.sh
+++ build_windows_10.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-packer build --only=$1-iso \
-    --var disk_size=262144 \
-    --var iso_url=./iso/Windows.iso \
-    --var iso_checksum=sha256:DDF91DF5BDF2230446EB77A959B7555C59D79F6BC7C622F7E16D31A9BA34A5BF \
+packer build --force \
+    --only=$1-iso \
     windows_10.json
